9b640a943e114f1efd13012fd433ff848fc35cf0
Martin Törnqvist <m.tornq@gmail.com>
2015-06-16 22:52:21 +0200

    Basic command line arguments handling

--- run_mutation_tests.py
+++ run_mutation_tests.py
@@ -6,10 +6,39 @@ Main module
 
 import sys
 
+import argparse
+
 import mutation_operators.logical
 import mutation_operators.statement_deletion
 
+PARSER = argparse.ArgumentParser(description='Mutation tester for C/C++.')
+
+PARSER.add_argument('--src-root',
+                    help='Path to root directory of your project (e.g. '
+                    'a Git repo)',
+                    required=True)
+
+PARSER.add_argument('--file-list',
+                    help='Path to a user provided file which should contain '
+                    'a list of source files to mutate',
+                    required=True)
+
+PARSER.add_argument('--test-hook',
+                    help='Path to a user provided test execution shell script '
+                    '(typically running a Make command)',
+                    required=True)
+
+ARGS = PARSER.parse_args()
+
+SRC_ROOT_PATH  = ARGS.src_root
+SRC_LIST_PATH  = ARGS.src_list
+TEST_HOOK_PATH = ARGS.test_hook
+
 def main():
+    '''
+    Entry point
+    '''
+
     print 'main()'
 
     mutator = mutation_operators.logical.Mutator()
@@ -21,7 +50,6 @@ def main():
     mutator = mutation_operators.statement_deletion.Mutator()
 
     mutator.run()
-
 
 if __name__ == "__main__":
     sys.exit(main())
